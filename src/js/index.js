3525f8bf8f5065c89f84c9e59ff495c102848bfb
weliomar <weliomarf@gmail.com>
2023-07-20 21:14:00 -0300

    Add files via upload

--- src/js/index.js
+++ src/js/index.js
@@ -23,7 +23,7 @@ personagens.forEach((personagem) => {
         const imagemPersonagemGrande = document.querySelector('.personagem-grande');
         
         const idPersonagem = personagem.attributes.id.value;
-        imagemPersonagemGrande.src = `./imagens/card-${idPersonagem}.png`;
+        imagemPersonagemGrande.src = `./src/imagens/card-${idPersonagem}.png`;
         
         const nomePersonagem = document.getElementById('nome-personagem')
         nomePersonagem.innerText = personagem.getAttribute('data-name');
